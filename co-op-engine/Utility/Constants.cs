47d903dda8a751aaeb85e844c4fbfe9d742cdd4f
Rick Batka <rick.batka@gmail.com>
2014-02-18 23:41:41 -0800

    animatedrenderer now listens for direction / state change events and reacts awesomely

---
+++ co-op-engine/Utility/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace co_op_engine.Utility
+{
+    public static class Constants
+    {
+        public static readonly int South = 0;
+        public static readonly int West = 1;
+        public static readonly int North = 2;
+        public static readonly int East = 3;
+    }
+    public enum ActorState
+    {
+        Idle = 0,
+        Walking = 1
+    }
+}
