b91f1233964accc09ff79ba1c1ba6ca13550d0ed
sean-surf <seanmarek@gmail.com>
2014-02-19 20:06:18 -0800

    gave the gamestate thing a try, had to revert... here's some unaffectes code

---
+++ co-op-engine/GameStates/GamePlay.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using co_op_engine.Collections;
+using co_op_engine.Components;
+using co_op_engine.Factories;
+using co_op_engine.ServiceProviders;
+using co_op_engine.Utility;
+using co_op_engine.World.Level;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace co_op_engine.GameStates
+{
+    class GamePlay : GameState
+    {
+        private Texture2D plainWhiteTexture;
+        private Texture2D towerTexture;
+        private Texture2D arrowTexture;
+        private Texture2D knightTexture;
+
+        ObjectContainer container;
+
+        //List<GameObject> Players = new List<GameObject>();
+        //List<GameObject> Enemies = new List<GameObject>();
+        //List<GameObject> Towers = new List<GameObject>();
+
+        //GameObject devPlayerObject;
+        //ElasticQuadTree tree;
+
+        public static Texture2D DEBUG_GRID_TEXTURE;
+
+        public GamePlay(Game1 game)
+            : base(game)
+        {
+            container = new ObjectContainer(GameRef.screenRectangle);
+        }
+
+        public override void LoadContent()
+        {
+            RegisterServices();
+
+
+            DEBUG_GRID_TEXTURE = GameRef.Content.Load<Texture2D>("grid");
+
+            plainWhiteTexture = GameRef.Content.Load<Texture2D>("pixel");
+            plainWhiteTexture.SetData<Color>(new Color[] { Color.White });
+            arrowTexture = GameRef.Content.Load<Texture2D>("arrow");
+            towerTexture = GameRef.Content.Load<Texture2D>("tower");
+            knightTexture = GameRef.Content.Load<Texture2D>("ww");
+
+            var animations = AnimationSet.BuildFromAsset("content/ww.txt");
+
+            ///////////////////////////////////////////////////////////
+
+            //@TODO move to level setup
+            PlayerFactory.GetPlayer(container, knightTexture, animations);
+
+            //Players.Add(PlayerFactory.GetPlayer(this, tree, knightTexture, new TileSheet(animation)));
+
+            ////@TODO EnemyFactory
+            //var devEnemy = new GameObject();
+            ////devEnemy.SetBrain(new StepFollow(devEnemy));
+            //devEnemy.SetBrain(new DoNothingBrain(devEnemy));
+            //devEnemy.SetupDevTempComponents(arrowTexture, tree);
+            //Enemies.Add(devEnemy);
+
+            TowerFactory.GetDoNothingTower(container, towerTexture, plainWhiteTexture);
+
+            //var devTower = TowerFactory.GetDoNothingTower(this, tree, towerTexture, plainWhiteTexture);
+            //Towers.Add(devTower);
+
+        }
+
+        private void RegisterServices()
+        {
+            GameServicesProvider.AddService(typeof(IActorInformationProvider), GameRef);
+            GameServicesProvider.AddService(typeof(IGraphicsInformationProvider), GameRef);
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            GameTimerManager.Instance.Update(gameTime);
+            container.UpdateAll(gameTime);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            GameRef.SpriteBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointWrap, null, null);
+
+            container.DrawAll(GameRef.SpriteBatch);
+            container.DebugDraw(GameRef.SpriteBatch, DEBUG_GRID_TEXTURE);
+
+            GameRef.SpriteBatch.End();
+        }
+
+        public List<GameObject> GetPlayers()
+        {
+            return container.GetPlayers();
+        }
+    }
+}
---
+++ co-op-engine/GameStates/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace co_op_engine.GameStates
+{
+    public abstract class GameState
+    {
+        protected Game1 GameRef;
+
+        public GameState(Game1 game)
+        {
+            this.GameRef = game;
+        }
+
+        public abstract void LoadContent();
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime);
+    }
+}
