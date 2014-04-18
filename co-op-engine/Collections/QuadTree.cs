ff9082bbc0974f5b1152ab10d571867b67065d29
sean-surf <seanmarek@gmail.com>
2014-04-17 19:53:29 -0800

    stubbed new quadtree, might start implementing tonight

---
+++ co-op-engine/Collections/QuadTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using co_op_engine.Components;
+using co_op_engine.Utility;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace co_op_engine.Collections
+{
+    public class QuadTree
+    {
+        public QuadTree(RectangleFloat bounds, QuadTree parent)
+        { }
+
+        public void MasterInsert(GameObject queryBounds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<GameObject> MasterQuery(RectangleFloat queryBounds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<GameObject> MasterQuery(Rectangle queryBounds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(GameObject newObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D drawTexture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(GameObject newObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int ChildCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<GameObject> GatherAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Verify()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<GameObject> Query(RectangleFloat query)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Split()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InflateBoundry(GameObject newObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotfyOfMovement(GameObject ownedObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
