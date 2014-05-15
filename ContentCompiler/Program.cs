95b7924608f1df51d5f703ba40ad81fc31473f8f
reddenx <seanmarek@gmail.com>
2014-05-14 20:36:57 -0700

    re-org, content compilation ready, projects shuffled

--- ContentCompiler/Program.cs
+++ ContentCompiler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ContentCompiler.ContentCompilation;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,23 +10,52 @@ namespace ContentCompiler
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             if (args.Length >= 2)
             {
                 string inputDirectory = args[0];
                 string outputDirectory = args[1];
 
-                ContentBuilder builder = new ContentBuilder();
-                builder.Clear();
-                builder.OnOutput += WriteLine;
-                builder.BuildAssets(inputDirectory, outputDirectory, false);
-                builder.OnOutput -= WriteLine;
+                if (!Directory.Exists(outputDirectory))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(outputDirectory);
+                    }
+                    catch
+                    {
+                        return 1;
+                    }
+                }
+                else if(Directory.Exists(inputDirectory))
+                {
+                    Console.WriteLine(string.Format("Compiling\n{0}\nto\n{1}",inputDirectory, outputDirectory));
+                    ContentBuilder builder = new ContentBuilder();
+                    builder.Clear();
+                    builder.OnOutput += WriteLine;
+                    builder.BuildAssets(inputDirectory, outputDirectory, false);
+                    builder.OnOutput -= WriteLine;
+
+                    DirectoryInfo inputDirectoryInfo = new DirectoryInfo(inputDirectory);
+
+                    foreach (FileInfo filename in inputDirectoryInfo.GetFiles("*.txt"))
+                    {
+                        filename.CopyTo(outputDirectory + "\\" + filename.Name, true);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input directory does not exist");
+                    return 1;
+                }
             }
             else
             {
                 Console.WriteLine("Usage: ContentCompiler.exe [ ? | inputDirectory outputDirectory ]");
+                return 1;
             }
+            return 0;
         }
 
         private static void WriteLine(object sender, ContentBuildEventArgs e)
