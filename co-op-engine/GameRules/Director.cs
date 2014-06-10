b5dae96f37ca943a1f03b888e86d7c051ee1fd01
reddenx <seanmarek@gmail.com>
2014-06-09 22:01:20 -0700

    added gamerules and some notes, next step stubbing

---
+++ co-op-engine/GameRules/Director.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace co_op_engine.GameRules
+{
+    class Director
+    {
+        //things the director needs to be aware of:
+        //hits
+        //kills
+        //damage
+        //spawns
+        //errors
+        //network
+
+        //actions:
+        //spawning, directly or setting up systems and letting them go
+        //resource monitoring (towers)
+        //pausing
+        //connection management(networking)
+        //saving/loading maybe
+        //evaluating win/lose and triggering correct response
+
+        //things that talk to this:
+        //effects doing damage/killing
+        //players constructing towers
+        //spawners notify of spawn
+        //error recovery from EVERYWHERE? maybe this should be in a logging class
+        //network conenction, add / remove, handle adjustment
+
+    }
+}
---
+++ co-op-engine/GameRules/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace co_op_engine.GameRules
+{
+    class Spawner
+    {
+        //used by
+        //director, directly(spawn a boss now) or setup and go(do this equation until I say stop)
+        //listens for stop spawning command
+
+        //uses
+        //factories to construct objects and set them out
+
+        //internal:
+        //contians an equation to direct spawning (thinking area under curve as inverse max time, most likely sinus in nature combined with other spawners to have harmonic spawns
+        //containes a timer of when to stop spawning for wave completion? or listens for stop command
+        //this would be a location to construct squads and setup their intercommunication
+    }
+}
